210b88bf1a2699e0be3eab39f6ca7df5f154b8c2
DSM4 <fatec-dsm4@academico.intra>
2025-08-18 22:28:26 -0300

    Melhorias no código

--- ldw-aula-01-introducao-flask/app.py
+++ ldw-aula-01-introducao-flask/app.py
@@ -14,7 +14,21 @@ def home(): # Função que será executada ao acessar a rota
 # Criando a rota '/games'
 @app.route('/games')
 def games():
-    return render_template('games.html')
+    title = 'Tarisland'
+    year = 2022
+    category = 'MMORPG'
+    # Lista em Python (array)
+    players = ['Yan', 'Ferrari', 'Valéria', 'Amanda']
+    # Dicionário em Python (objeto)
+    console = {'Nome' : 'Playstation 5',
+               'Fabricante': 'Sony',
+               'Ano': 2020}
+    return render_template('games.html', 
+                           title=title,
+                           year=year,
+                           category=category,
+                           players=players,
+                           console=console)
 
 
 # Se for executado diretamente pelo interpretador
